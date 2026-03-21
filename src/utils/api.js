ebefca47edd3aea145b66c60db6fef0ad196fff7
Raajasimman-maker <raajasimmang15@gmail.com>
2026-03-21 13:55:59 +0530

    updated api url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://balaji-backend-7dos.onrender.com',
   headers: {
     'Content-Type': 'application/json'
   }
